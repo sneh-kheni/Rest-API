e72387262018249b2bffc5131bdc93513ace47d3
Sneh kheni <snehkheni54@gmail.com>
2025-11-13 09:25:15 +0530

    Add files via upload

---
+++ gemini_chatbot.py
@@ -0,0 +1,19 @@
+import google.generativeai as genai
+import os
+os.environ["GRPC_VERBOSITY"] = "NONE"
+os.environ["GLOG_minloglevel"] = "2"
+
+genai.configure(api_key="AIzaSyDUBHj48GewPtzog0PxxGRsrFZ-X_eqtyw")
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+
+
+while True:
+    prompt = input("You: ")
+    response = model.generate_content(prompt)
+    if prompt=='quit':
+        break
+    txt = str(response.text)
+    txt = txt.replace("*", "")
+    print(f"AI:{txt.strip()}")
